0d8186c7112759389fe93f9817adfa7b891ca600
Sidharath Mankani <mankanisidharath@gmail.com>
2022-03-09 16:21:30 +0530

    Created UsersController

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit(User $user)
+    {
+
+    }
+
+    public function update(Request $request, User $user)
+    {
+
+    }
+
+    public function destroy(User $user)
+    {
+
+    }
+}
