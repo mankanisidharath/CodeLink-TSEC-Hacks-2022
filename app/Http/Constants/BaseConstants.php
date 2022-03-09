c5f2f9607136be5a80d826c25e17713e7f932d19
Sidharath Mankani <mankanisidharath@gmail.com>
2022-03-09 16:21:12 +0530

    Created BaseConstants for general usage constants

---
+++ app/Http/Constants/BaseConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Constants;
+
+interface BaseConstants
+{
+    const STATUS_ACTIVE = "ACTIVE";
+    const STATUS_INACTIVE = "INACTIVE";
+    const STATUS = [
+        'ACTIVE',
+        'INACTIVE'
+    ];
+}
