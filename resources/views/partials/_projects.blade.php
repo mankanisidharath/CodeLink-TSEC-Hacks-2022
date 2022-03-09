c969f1857c0da9afff24bcd4317a3f3a10fd67be
Mihir Karira <karira.mihir@gmail.com>
2022-03-10 00:48:36 +0530

    created _projects.blade partial file

---
+++ resources/views/partials/_projects.blade.php
@@ -0,0 +1,14 @@
+<section id="projects" class="section m-16 mx-32">
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-between">
+            <div class="col-6">
+                <div class="text-2xl font-bold inline-block">Recommended for you</div>
+            </div>
+            <div class="col-6 d-flex justify-content-end">
+                <div class="search-field inline-flex">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    <input type="text" class="form-control" placeholder="search">
+                </div>
+            </div>
+        </div>
+</section>
