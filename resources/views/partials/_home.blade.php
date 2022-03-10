3b8b3528d2c21a18c0a589baa951a638d407b6d4
Mihir Karira <karira.mihir@gmail.com>
2022-03-10 10:08:18 +0530

    implemented modal html

--- resources/views/partials/_home.blade.php
+++ resources/views/partials/_home.blade.php
@@ -3,13 +3,13 @@
         <div class="row items-center">
             <div class="col-12 col-md-8">
                 <div class="flex flex-col">
-                    <div class="text-xl font-bold ">Code Link</div>
-                    <div class="text-4xl font-black">Connecting developers</div>
+                    <div class="text-xl font-bold color-blue">Code Link</div>
+                    <div class="text-4xl font-black color-blue">Connecting developers</div>
                     <div class="text-base mt-2">CodeLink is a coding community platform where developers can collaborate and build awesome projects!</div>
                 </div>
                 <div class="flex mt-4 items-center">
                     <a href="" class="py-2 px-4 primary-blue rounded transition duration-300">Connect</a>
-                    <a href="" class="ms-4 color-blue hover:underline">Add Project</a>
+                    <a href="" class="ms-4 color-blue hover:underline" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Project</a>
                 </div>
             </div>
 
@@ -273,4 +273,26 @@
 
             </div>
         </div>
+    </div>
+    <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Your Project Link</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Git Link </label>
+                            <input type="" class="form-control" id="exampleFormControlInput1" placeholder="https://github.com/">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Add it</button>
+                    </div>
+                </div>
+                </div>
+            </div>
 </section>
