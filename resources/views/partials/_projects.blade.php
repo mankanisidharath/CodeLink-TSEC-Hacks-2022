9f7d3a5d4d934e100bab1a2d4db52add732e1bbe
Mihir Karira <karira.mihir@gmail.com>
2022-03-10 10:08:43 +0530

    added card section

--- resources/views/partials/_projects.blade.php
+++ resources/views/partials/_projects.blade.php
@@ -11,4 +11,19 @@
                 </div>
             </div>
         </div>
+        <div class="row d-flex align-items-center mt-16">
+            <div class="col-6">
+                <div class="card w-16" style="width: 18rem;">
+                    <div class="card-body d-flex">
+                        <img src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=20&m=476085198&s=170667a&w=0&h=FXkT-N6vISLOCUefa9MyQg0pH-6loMX9zBZjgLK458c=" alt="" class="w-16">
+                        <h5 class="card-title">Jennifer Williamson</h5>
+                        <div class="flex">
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
+
